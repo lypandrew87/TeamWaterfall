fed5e5b15e4fa67e02c72a305cff21cfa7f43308
smjenniges <drjennig@hotmail.com>
2017-03-20 16:02:57 -0500

    Update loginsuccessful.php
    Added button to create new project

--- loginsuccessful.php
+++ loginsuccessful.php
@@ -1,41 +1,90 @@
-
-<!DOCTYPE html>
-<html >
-<head>
-  <link rel="stylesheet" href="css/style.css">
-
-  <meta charset="UTF-8">
-
-
-  <?php
-   session_start(); 
-
-   $firstName =  $_SESSION["firstname"]; 
-   $lastName = $_SESSION["lastname"]; 
-   $username =  $_SESSION["username"]; 
-   $password = $_SESSION["password"];
-
-   
- 
-  
-  ?>
-  <title>Team Waterfall</title>
-</head>
-
-<body>
-
-<p> Welcome: <?php echo $firstName . " " . $lastName; ?></p>
-
-
-
-<form action ="board.php">
-
-<input type ="submit" value = "Project Board"> 
-</form>
-
-<form action ="index.php">
-
-<input type ="submit" value = "Logout"> 
-</form>
-</body>
-</html>
+
+
+<!DOCTYPE html>
+
+<html >
+
+<head>
+
+  <link rel="stylesheet" href="css/style.css">
+
+
+
+  <meta charset="UTF-8">
+
+
+
+
+
+  <?php
+
+   session_start(); 
+
+
+
+   $firstName =  $_SESSION["firstname"]; 
+
+   $lastName = $_SESSION["lastname"]; 
+
+   $username =  $_SESSION["username"]; 
+
+   $password = $_SESSION["password"];
+
+
+
+   
+
+ 
+
+  
+
+  ?>
+
+  <title>Team Waterfall</title>
+
+</head>
+
+
+
+<body>
+
+
+
+<p> Welcome: <?php echo $firstName . " " . $lastName; ?></p>
+
+
+
+
+
+
+
+<form action ="board.php">
+
+
+
+<input type ="submit" value = "Project Board"> 
+
+</form>
+
+
+<form action ="createProject.php">
+
+
+
+<input type ="submit" value = "New Project"> 
+
+</form>
+  
+  
+<form action ="index.php">
+
+
+
+<input type ="submit" value = "Logout"> 
+
+</form>
+
+</body>
+
+</html>
+
