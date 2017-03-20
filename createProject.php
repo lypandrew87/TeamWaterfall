9f682098a7d3ca418dd71ef4f5b5ab9dcdb7f94d
smjenniges <drjennig@hotmail.com>
2017-03-20 16:01:40 -0500

    Added createProject.php

---
+++ createProject.php
@@ -0,0 +1,56 @@
+//SMJ 3-20 - Pretty much an exact copy of create account with the fields changed
+ <?php
+
+//This means that there was an incorrect password
+
+if(isset($_GET['uError'])){
+
+
+
+?>
+
+<p  style = "color: red"> This project name is not unique! please try a different name </p>
+
+
+
+<?php 
+
+}
+
+?>
+
+
+
+
+
+<link rel="stylesheet" href="css/style.css">
+
+
+
+  <div class="form-style-2">
+
+
+
+
+
+<form method="post" action = "phpHandling.php">
+
+  Project Name:<br>
+
+  <input  type="text" name="projectname" required ><br><br>
+
+  <input type="submit" value="Submit">
+
+  <input type="reset">
+
+</form>
+
+
+
+
+
+</div>
+
+
+
+
